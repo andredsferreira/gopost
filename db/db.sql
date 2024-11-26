7def72038116355076d608d42f4e3016487389b3
andredsferreira <andredsferreira2@gmail.com>
2024-11-26 19:07:14 +0000

    Create post endpoint.

--- db/db.sql
+++ db/db.sql
@@ -3,6 +3,7 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE
 );
+
 CREATE TABLE posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255),
--- handler/handler.go
+++ handler/handler.go
@@ -12,7 +12,18 @@ import (
 var tmpl = template.Must(template.ParseGlob("static/templates/*.html"))
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl.ExecuteTemplate(w, "index", nil)
+	c, err := r.Cookie("jwt")
+	isLoggedIn := false
+	if err == nil {
+		_, err := service.GetClaimsFromJWT(c.Value)
+		if err == nil {
+			isLoggedIn = true
+		}
+	}
+	data := map[string]interface{}{
+		"IsLoggedIn": isLoggedIn,
+	}
+	tmpl.ExecuteTemplate(w, "index", data)
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -99,7 +110,26 @@ func GetUserPostsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreatePostHandler(w http.ResponseWriter, r *http.Request) {
-
+	title := r.FormValue("title")
+	content := r.FormValue("content")
+	c, err := r.Cookie("jwt")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+	claims, err := service.GetClaimsFromJWT(c.Value)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	username := claims["username"].(string)
+	err = model.CreatePost(username, title, content)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotAcceptable)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Post successfully created")
 }
 
 func RenderCreatePostHandler(w http.ResponseWriter, r *http.Request) {
